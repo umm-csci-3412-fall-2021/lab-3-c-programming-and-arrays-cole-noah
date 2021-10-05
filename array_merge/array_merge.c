3dfbfa4c06bb50a76fc8fce3a6f4aad4935a65db
cole-maxwell1 <cole.n.maxwell99@gmail.com>
2021-10-05 11:33:55 -0500

    Add main for testing

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -10,7 +10,8 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
   //Iterate through the sizes[] to find the approximate final array size
   //(will not be exact because this does not account for unique integers)
   for (y  = 0; y < num_arrays; y++){
-    finalSize =+ sizes[y];
+    finalSize += sizes[y];
+    // printf("Current total size = %d\n", finalSize);
   }
 
   //Create our result array using the finalSize integer obtained above
@@ -18,23 +19,30 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
 
   //Use the num_arrays variable and the sizes[] to iterate through values[]
   //and add each integer in values to our result array
+  int posInArray = 0;
   for (i  = 0; i < num_arrays - 1; i++){
     for (j = 0; j < sizes[i] - 1; j++) {
-      result[j] = values[i][j];
+      result[posInArray] = values[i][j];
+      posInArray++;
     }
   }
   
 
 
-  return sizes;
+  return result;
 }
 
 int main() {
   int num_arrays = 3;
   int sizes[] = { 4, 2, 5 };
-  int values[] = { { 3, 2, 0, 5 }, { 8, 9 }, { 6, 3, 2, 0, 5 } };
+  int a0[] = { 3, 2, 0, 5 };
+  int a1[] = { 8, 9 };
+  int a2[] = { 6, 3, 2, 0, 5 };
+  int* values[] = { a0, a1, a2 };
+  int* resultArray;
 
-  array_merge(num_arrays, sizes, values);
+  resultArray = array_merge(num_arrays, sizes, values);
+  printArray(resultArray, 11);
 
   return 0;
 }
